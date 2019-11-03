c26aafab21761a7827714edee8deff675c035f75
liulong <706750966@qq.com>
2019-11-03 20:35:03 +0800

    spring-cloud-alibaba finish
    Signed-off-by: liulong <706750966@qq.com>

--- hello-spring-cloud-alibaba-consumer/src/main/java/com/funtl/spring/cloud/alibaba/consumer/controller/TestEchoController.java
+++ hello-spring-cloud-alibaba-consumer/src/main/java/com/funtl/spring/cloud/alibaba/consumer/controller/TestEchoController.java
@@ -2,6 +2,7 @@ package com.funtl.spring.cloud.alibaba.consumer.controller;
 
 import com.funtl.spring.cloud.alibaba.consumer.service.EchoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 @RestController
 public class TestEchoController {
+
     @Autowired
     private EchoService echoService;
     @GetMapping("/feign/echo/{str}")
